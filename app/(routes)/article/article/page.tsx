3426468caf9d558b6d6fe7e456fb0a08b903552a
Amit-Biswas-WD <amitbiswas9282@gmail.com>
2026-04-21 02:59:56 +0600

    feat: add ArticleProductCard component and article images, update article page
    - update article page (page.tsx)
    - add ArticleProductCard component
    - include article product image asset

--- app/(routes)/article/article/page.tsx
+++ app/(routes)/article/article/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { ArticleBanner } from "@/components/articles/ArticleBanner";
+import { ArticleProductCard } from "@/components/articles/ArticleProductCard";
 
 export default function ArticlePage() {
   return (
     <div>
       <ArticleBanner />
+      <ArticleProductCard />
     </div>
   );
 }
